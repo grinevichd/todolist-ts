95ace9997060c4b295dc124a4a9c82ab132a6adf
Dima <d2.grinevich@gmail.com>
2022-11-22 16:12:32 +0300

    Updates

---
+++ precache-manifest.a76b3ccc79e33109b501bc51c5a0c1be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4a2db48c2c54b2dbd9e9cc94eb78545",
+    "url": "/todolist-ts/index.html"
+  },
+  {
+    "revision": "d2e31e43dcdc9f8b36bb",
+    "url": "/todolist-ts/static/css/main.155097fa.chunk.css"
+  },
+  {
+    "revision": "fcf43a3ec2c5bf61f8fb",
+    "url": "/todolist-ts/static/js/2.f9af47a9.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/todolist-ts/static/js/2.f9af47a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2e31e43dcdc9f8b36bb",
+    "url": "/todolist-ts/static/js/main.57d27a0d.chunk.js"
+  },
+  {
+    "revision": "8bdf15c7027486d2fcd1",
+    "url": "/todolist-ts/static/js/runtime-main.ca93a274.js"
+  }
+]);
